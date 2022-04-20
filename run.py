ae080ce994cd56d85d0f80537037d03d9b84ec23
Peter Hennessy <peternesta@hotmail.com>
2022-04-20 09:06:16 +0000

    Major problem with indentation errors, will try to fix

--- run.py
+++ run.py
@@ -1,18 +1,42 @@
 import random
 def hangman () :
-    """
-    Create random list pf words for the computer to choose from
-    """
-
-    list_of_words = ('hi', 'cat', 'home', 'country', 'computer', 'phone')
+       
+    list_of_words = ['hi', 'cat', 'home', 'country', 'computer', 'phone']
     word = random.choice(list_of_words)
-    print(word)
-    
-     
+    attempts = 5
+    guessmade = ''
+    valid_entry = set ('abcdefghijklmnopqrstuvwxyz')
+
+while len(word) > 0:
+        main_word = ""
+        missed = 0
+ 
+      
+for letter in word:
+       if letter in guessmade:
+        main_word = main_word +letter
+
+       else:
+        main_word = main_word + "_ "
+       if main_word == word:
+            print(main_word)
+            print("You have been granted a pardon!!!!")
+            break
+          
+
+       print("guess the words ", main_word)
+       guess = input()
+
+       if guess in valid_entry:
+        guessmade = guessmade + guess 
+       else:
+        print("Enter valid character") 
+        guess = input() 
+
 # Welcome the Player and ask to enter their name
 name = input("Please enter your name gallowman: -->")
 print("Welcome", name,"Who's for hanging today!")
-print("========================")
+print("=======================================")
 print("Try to guess in less than 5 attempts")
 hangman()
 
